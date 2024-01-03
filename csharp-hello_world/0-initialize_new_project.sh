89eceeb3b66742c658092a18974d55d392daaed3
ZAKosma <zakosma@gmail.com>
2024-01-03 11:08:01 -0600

    mpdfiy

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Use the first argument as the project name. If not provided, default to "0-new_project"
-PROJECT_NAME=${1:-"0-new_project"}
+#PROJECT_NAME=${1:-"0-new_project"}
 
-dotnet new console -o "$PROJECT_NAME" -f "netcoreapp2.1"
+#dotnet new console -o "$PROJECT_NAME" -f "netcoreapp2.1"
+dotnet new console -o ${1:-"0-new_project"} -f "netcoreapp2.1"
