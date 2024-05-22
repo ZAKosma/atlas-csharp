b1f000dea93be0571f78b074df0a6e792561e817
Zachary Kosma <contact@zak.today>
2024-05-22 08:56:05 -0500

    Update 0-universal_health.cs

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,28 +1,34 @@
-﻿﻿using System;
+using System;
+
 
 class Player
 {
-    private string name { get; set; }
-    private float maxHp { get; set; }
-    private float hp { get; set; }
+    private string name;
+    private float maxHp;
+    private float hp;
 
+    /// <summary>
+    /// Player constructor
+    /// </summary>
+    /// <param name="name">Players name</param>
+    /// <param name="maxHp">total health</param>
     public Player(string name = "Player", float maxHp = 100f)
     {
-        if (maxHp <= 0)
-        {
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
-        }
+        this.name = name;
+        if (maxHp > 0) this.maxHp = maxHp;
         else
         {
-            this.maxHp = maxHp;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
         }
-
         this.hp = this.maxHp;
-        this.name = name;
     }
-    
+
+    /// <summary>
+    /// Prints the health of Player
+    /// </summary>
     public void PrintHealth()
     {
-        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
     }
 }
